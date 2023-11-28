f54d2d3b4a36e80ddc709cc4b7d935fc419c634c
adityabajpayee <adityabajpayeesln@gmail.com>
2023-11-28 22:41:03 +0530

    Someting added in Car.java

--- Car.java
+++ Car.java
@@ -1,8 +1,9 @@
-class Car{
+Class Car{
   public static void main(String []Args){
     System.out.println("Something in java");
     for(int i = 0; i <= 10; i++){
       System.out.println("no is" + i);
     }
+      System.out.println("adding something");
   }
 }
