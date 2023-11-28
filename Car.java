2f1d03140d16869ec8d8435264138414e94bae63
Aditya Bajpayee <110543768+aditya-bajpayee@users.noreply.github.com>
2023-11-28 22:33:03 +0530

    Update for loop

--- Car.java
+++ Car.java
@@ -1,5 +1,8 @@
 class Car{
   public static void main(String []Args){
     System.out.println("Something in java");
+    for(int i = 0; i <= 10; i++){
+      System.out.println("no is" + i);
+    }
   }
 }
